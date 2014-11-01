2ae64ae8f3d1cc29c951b55b8976eb5c5c678fdf
Rafael Oshiro <rafael.oshiro@gmail.com>
2014-11-01 15:09:55 +0000

    connecting to mongolab

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ var http = require('http'),
     mongoPort = 27017,
     mongoClient = new MongoClient(new Server(mongoHost, mongoPort));
 
+var uri = "mongodb://heroku_app31135802:heroku_app31135802@ds049150.mongolab.com:49150/heroku_app31135802"
+// var uri = "mongodb://localhost:27017/heroku_app31135802"
+
 // Opens connection with Mongo DB
-mongoClient.open(function(err, mongoClient) {
-  if (!mongoClient) {
-    console.error("Error! Exiting... Must start MongoDB first");
-    process.exit(1);
-  }
-  var db = mongoClient.db("Openws");
+MongoClient.connect(uri, function(err, db) {
+  // if (!mongoClient) {
+  //   console.error("Error! Exiting... Must start MongoDB first");
+  //   process.exit(1);
+  // }
+  if(err) throw err;
+  // var db = mongoClient.db("Openws");
   collectionDriver = new CollectionDriver(db);
   console.info("Database connection OK");
 });
