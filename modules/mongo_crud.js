b0e5f41586f688950841c4a2d38bfef31b8490d2
Rafael Oshiro <rafael.oshiro@gmail.com>
2014-12-01 00:08:33 +0000

    Restricting users to their own namespaces

--- modules/mongo_crud.js
+++ modules/mongo_crud.js
@@ -1,21 +1,50 @@
 (function() {
   var mongo = require('mongodb');
   var hat   = require('hat');
+  var Promise = require('node-promise').Promise;
+  var when  = require('node-promise').when;
 
   var _apiKey = function(req) {
     return req.param("apiKey");
   },
 
+  _errorHandler = function(response, error) {
+    response.send(400,{msg: error});
+  },
+
   _getNamespace = function(req, res) {
     var apiKey = _apiKey(req);
-    if(apiKey && apiKey.length == 0) {
-      // Error
+    var promise = new Promise();
+
+    console.log("APIKey: " + apiKey);
+    if(apiKey && apiKey.length > 0) {
+      // TODO: Refactor to retrieve from DB
+      if(apiKey == "b9276e788652f8287e80242dab6e5a2a") {
+        promise.resolve("app");
+      } else {
+        collectionDriver.find("app.users", {api_key: apiKey}, function(err, objs) {
+          if(err) promise.reject(err);
+
+          if(objs && objs.length > 0) {
+            promise.resolve(objs[0].email.replace('@','_'));
+          } else {
+            promise.reject("Invalid API Key");
+          }
+        });
+      }
+      return promise;
     }
-    return "app";
   },
 
   _collection = function(req, res) {
-    return _getNamespace(req, res) + "." + req.params.collection;
+    var promise = new Promise();
+    when(_getNamespace(req, res), function(namespace) {
+      promise.resolve(namespace + "." + req.params.collection);
+    },
+    function(err) {
+      _errorHandler(res, err);
+    });
+    return promise;
   },
 
   _generateApiKey = function() {
@@ -26,13 +55,16 @@
     var user = req.body;
     console.log("User signup/login request: " + user.email);
     collectionDriver.find("app.users", { user_id: user.user_id }, function(errors, objs) {
+      if(errors) throw errors;
+
       if(objs.length > 0) {
         res.send(200, objs[0]);
       } else {
         user["api_key"] = _generateApiKey();
         collectionDriver.save("app.users", user, function(err, docs) {
-          if (err) { res.send(400, err); }
-          else { res.send(201, docs); }
+          if (err) throw err;
+
+          res.send(201, docs);
         });
       }
     });
@@ -41,75 +73,103 @@
   exports.findAll = function(req, res) {
     var params = req.params;
     var query = req.param("q");
-    var callback = function(error, objs) {
-      if (error) {
-        res.send(400, error);
+
+    var callback = function(err, objs) {
+      if (err) {
+        throw err;
       } else {
         res.set('Content-Type','application/json');
         res.send(200, objs);
       }
     };
 
-    if(query) {
-      collectionDriver.find(_collection(req,res), JSON.parse(query), callback);
-    } else {
-      collectionDriver.findAll(_collection(req,res), callback);
-    }
+    when(_collection(req,res),
+      function(coll) {
+        console.log("The coll is: " + coll);
+        if(query) {
+          collectionDriver.find(coll, JSON.parse(query), callback);
+        } else {
+          collectionDriver.findAll(coll, callback);
+        }
+      },
+      function(err) {
+        _errorHandler(res, err);
+      }
+    );
   };
 
   exports.get = function(req, res) {
     var params = req.params;
     var entity = params.entity;
 
-    if (entity) {
-      collectionDriver.get(_collection(req,res), entity, function(error, objs) {
-        if (error) { res.send(400, error); }
-        else { res.send(200, objs); }
-      });
-    } else {
-      res.send(400, {error: 'bad url', url: req.url});
-    }
+    when(_collection(req,res),
+      function(coll) {
+        if(entity) {
+          collectionDriver.get(coll, entity, function(error, objs) {
+            if (error) { throw errors; }
+            else { res.send(200, objs); }
+          });
+        }
+      }, function(err) {
+        _errorHandler(res, err);
+      }
+    );
   };
-
-  exports.find = function(req, res) {
-    var query = req.param("q");
-    console.log(query);
-  },
 
   exports.create = function(req, res) {
     var object = req.body;
-    collectionDriver.save(_collection(req,res), object, function(err,docs) {
-      if (err) { res.send(400, err); }
-      else { res.send(201, docs); }
+    when(_collection(req,res), function(coll) {
+      collectionDriver.save(coll, object, function(err,docs) {
+        if (err) { res.send(400, err); }
+        else { res.send(201, docs); }
+      });
+    },
+    function(err) {
+      _errorHandler(res, err);
     });
   };
 
   exports.update = function(req, res) {
     var params = req.params;
     var entity = params.entity;
-    if (entity) {
-      collectionDriver.update(_collection(req,res), req.body, entity, function(error, objs) { //B
-        if (error) { res.send(400, error); }
-        else { res.send(200, objs); }
-      });
-     } else {
-       var error = { "message" : "Cannot PUT a whole collection" };
-       res.send(400, error);
-     }
+
+    when(_collection(req,res),
+      function(coll) {
+        if (entity) {
+          collectionDriver.update(coll, req.body, entity, function(err, objs) {
+            if (err) { res.send(400, err); }
+            else { res.send(200, objs); }
+          });
+        } else {
+          var error = { "message" : "Cannot PUT a whole collection" };
+          res.send(400, error)
+        }
+      },
+      function(err) {
+        _errorHandler(res, err);
+      }
+    );
   };
 
   exports.delete = function(req, res) {
     var params = req.params;
     var entity = params.entity;
-    if (entity) {
-      collectionDriver.delete(_collection(req,res), entity, function(error, objs) { //B
-        if (error) { res.send(400, error); }
-        else { res.send(200, objs); }
-      });
-    } else {
-       var error = { "message" : "Cannot DELETE a whole collection" };
-       res.send(400, error);
-     }
+
+    when(_collection(req,res), function(coll) {
+        if (entity) {
+          collectionDriver.delete(coll, entity, function(err, objs) { //B
+            if (error) { res.send(400, err); }
+            else { res.send(200, objs); }
+          });
+        } else {
+          var error = { "message" : "Cannot DELETE a whole collection" };
+          res.send(400, error);
+        }
+      },
+      function(err) {
+        _errorHandler(res, err);
+      }
+    );
   };
 
 })();
